762847f79bebb49eacdb0eb268fcdf3e7f716278
thebearodactyl <dabewodactowluwu@gmail.com>
2024-03-12 17:05:47 -0500

    bad beans D:

--- src/main.cpp
+++ src/main.cpp
@@ -5,83 +5,90 @@
 using namespace geode::prelude;
 using namespace std::chrono;
 
+// thanks to shadowforce78
+void HSVtoRGB(float &fR, float &fG, float &fB, float &fH, float &fS, float &fV)
+{
+    float fC = fV * fS; // Chroma
+    float fHPrime = fmod(fH / 60.0, 6);
+    float fX = fC * (1 - fabs(fmod(fHPrime, 2) - 1));
+    float fM = fV - fC;
+
+    if (0 <= fHPrime && fHPrime < 1)
+    {
+        fR = fC;
+        fG = fX;
+        fB = 0;
+    }
+    else if (1 <= fHPrime && fHPrime < 2)
+    {
+        fR = fX;
+        fG = fC;
+        fB = 0;
+    }
+    else if (2 <= fHPrime && fHPrime < 3)
+    {
+        fR = 0;
+        fG = fC;
+        fB = fX;
+    }
+    else if (3 <= fHPrime && fHPrime < 4)
+    {
+        fR = 0;
+        fG = fX;
+        fB = fC;
+    }
+    else if (4 <= fHPrime && fHPrime < 5)
+    {
+        fR = fX;
+        fG = 0;
+        fB = fC;
+    }
+    else if (5 <= fHPrime && fHPrime < 6)
+    {
+        fR = fC;
+        fG = 0;
+        fB = fX;
+    }
+    else
+    {
+        fR = 0;
+        fG = 0;
+        fB = 0;
+    }
+
+    fR += fM;
+    fG += fM;
+    fB += fM;
+}
+
+// thanks to shadowforce78
 float g = 0;
+cocos2d::_ccColor3B getRainbow(float offset, float saturation)
+{
+    float R;
+    float G;
+    float B;
 
-void hsv_to_rgb(float &fR, float &fG, float &fB, float &fH, float &fS, float &fV) {
-	float fC = fV * fS;
-	float fHPrime = fmod(fH / 60.0, 6);
-	float fX = fC * (1 - fabs(fmod(fHPrime, 2) - 1));
-	float fM = fV - fC;
+    float hue = fmod(g + offset, 360);
+    float sat = saturation / 100.0;
+    float vc = 1;
+    HSVtoRGB(R, G, B, hue, sat, vc);
 
-	switch (static_cast<int>(fHPrime))
-	{
-	case 0:
-		fR = fC;
-		fG = fX;
-		fB = 0;
-		break;
-	case 1:
-		fR = fX;
-		fG = fC;
-		fB = 0;
-		break;
-	case 2:
-		fR = 0;
-		fG = fC;
-		fB = fX;
-		break;
-	case 3:
-		fR = 0;
-		fG = fX;
-		fB = fC;
-		break;
-	case 4:
-		fR = fX;
-		fG = 0;
-		fB = fC;
-		break;
-	case 5:
-		fR = fC;
-		fG = 0;
-		fB = fX;
-		break;
-	default:
-		fR = 0;
-		fG = 0;
-		fB = 0;
-		break;
-	}
-
-	fR += fM;
-	fG += fM;
-	fB += fM;
+    cocos2d::_ccColor3B out;
+    out.r = R * 255;
+    out.g = G * 255;
+    out.b = B * 255;
+    return out;
 }
 
-cocos2d::_ccColor3B get_rainbow(float offset, float saturation) {
-	float R, G, B;
-
-	float hue = fmod(g + offset, 360);
-	float sat = saturation / 100.0;
-	float vc = 1;
-	hsv_to_rgb(R, G, B, hue, sat, vc);
-
-	cocos2d::_ccColor3B out;
-	out.r = R * 255;
-	out.g = G * 255;
-	out.b = B * 255;
-	return out;
-}
-
+// thanks to shadowforce78
 class $modify(PlayerObject) {
-	void flashPlayer(float p0, float p1, cocos2d::ccColor3B mainColor, cocos2d::ccColor3B secondColor) {
-		// uwu
-	}
+    void flashPlayer(float p0, float p1, cocos2d::ccColor3B mainColor, cocos2d::ccColor3B secondColor){
+        // neutralised kappa
+    }
 };
 
 class $modify(PlayLayer) {
-	CCSprite *progressBar;
-	CCLabelBMFont *percentLabel;
-
 	void postUpdate(float p0) {
 		float speed = Mod::get()->getSettingValue<double>("speed");
 		float saturation = Mod::get() -> getSettingValue<double>("saturation");
@@ -92,9 +99,9 @@ class $modify(PlayLayer) {
 			g += speed / 10;
 		}
 
-		auto rainbowColor = get_rainbow(0, saturation);
-		auto rainbowColor2 = get_rainbow(180, saturation);
-		auto rainbowColor3 = get_rainbow(90, saturation);
+		auto rainbowColor = getRainbow(0, saturation);
+		auto rainbowColor2 = getRainbow(180, saturation);
+		auto rainbowColor3 = getRainbow(90, saturation);
 		bool enable = Mod::get()->getSettingValue<bool>("enable");
 
 		if (enable) {
@@ -105,6 +112,8 @@ class $modify(PlayLayer) {
 			if (m_player2->m_waveTrail) {
 				m_player2->m_waveTrail->setColor(rainbowColor2);
 			}
+
+			PlayLayer::postUpdate(p0);
 		}
 	}
 };
