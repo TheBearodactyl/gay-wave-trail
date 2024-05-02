c516e90d3855cb7764a2e5e9445aa9b65d6af0f1
The Motherfucking Bearodactyl <dabewodactowluwu@gmail.com>
2024-05-02 14:13:31 -0500

    optimize hsv_to_rgb even more\nthis time for more accuracy :3

--- src/trail_customization/rainbow_trail.cpp
+++ src/trail_customization/rainbow_trail.cpp
@@ -2,36 +2,47 @@
 #include <cmath>
 
 void RainbowTrail::hsv_to_rgb(float &fR, float &fG, float &fB, float &fH, float &fS, float &fV) {
-  float fC = fV * fS, fHPrime = fmodf(fH / 60.0f, 6), fX = fC * ( 1 - fabsf(fmodf(fHPrime, 2) - 1));
-  int iHue = static_cast<int>(fHPrime);
-  // ternary operators are fucking awesome.
-  fR = ( iHue == 0 )
-       ? fC
-       : ( iHue == 1 )
-         ? fX
-         : ( iHue == 4 )
-           ? fX
-           : ( iHue == 5 )
-             ? fC
-             : 0;
-  fG = ( iHue == 1 )
-       ? fC
-       : ( iHue == 2 )
-         ? fC
-         : ( iHue == 3 )
-           ? fX
-           : 0;
-  fB = ( iHue == 3 )
-       ? fC
-       : ( iHue == 4 )
-         ? fC
-         : ( iHue == 5 )
-           ? fX
-           : 0;
-  float fM = fV - fC;
-  fR += fM;
-  fG += fM;
-  fB += fM;
+  float c = fV * fS;
+  float x = static_cast<float>(static_cast<double>(c) * ( 1 - std::abs(std::fmod(fH / 60.0f, 2) - 1)));
+  float m = fV - c;
+
+  fR = ( fH < 60.0f )
+       ? c
+       : ( fH < 120.0f )
+         ? x
+         : ( fH < 180.0f )
+           ? 0
+           : ( fH < 240.0f )
+             ? 0
+             : ( fH < 300.0f )
+               ? x
+               : c;
+  fG = ( fH < 60.0f )
+       ? x
+       : ( fH < 120.0f )
+         ? c
+         : ( fH < 180.0f )
+           ? c
+           : ( fH < 240.0f )
+             ? x
+             : ( fH < 300.0f )
+               ? 0
+               : 0;
+  fB = ( fH < 60.0f )
+       ? 0
+       : ( fH < 120.0f )
+         ? 0
+         : ( fH < 180.0f )
+           ? x
+           : ( fH < 240.0f )
+             ? c
+             : ( fH < 300.0f )
+               ? c
+               : x;
+
+  fR += m;
+  fG += m;
+  fB += m;
 }
 
 float RainbowTrail::g = 0;
@@ -40,11 +51,11 @@ cocos2d::_ccColor3B RainbowTrail::get_rainbow(float offset, float saturation) {
   float R, G, B;
 
   float hue = static_cast<float>(fmod(g + offset, 360));
-  float sat = static_cast<float>(saturation / 100.0);
+  float sat = static_cast<float>(static_cast<double>(saturation) / 100.0);
   float vc = 1;
   hsv_to_rgb(R, G, B, hue, sat, vc);
 
-  cocos2d::_ccColor3B out;
+  cocos2d::_ccColor3B out{};
   out.r = static_cast<GLubyte>(R * 255);
   out.g = static_cast<GLubyte>(G * 255);
   out.b = static_cast<GLubyte>(B * 255);
