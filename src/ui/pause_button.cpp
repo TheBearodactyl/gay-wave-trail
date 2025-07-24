93d565c0f1bb8c457f50c7b932002f2fea75166b
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-07-24 13:50:15 -0500

    add pause menu button toggle

--- src/ui/pause_button.cpp
+++ src/ui/pause_button.cpp
@@ -1,35 +1,36 @@
 // Credits go to RayDeeUx for the pause button code :D
 
+#include "../settings/gay_settings.hpp"
 #include <Geode/modify/Modify.hpp>
 #include <Geode/modify/PauseLayer.hpp>
 #include <Geode/ui/GeodeUI.hpp>
 
 using namespace geode::prelude;
+using namespace gay::settings;
 
 class $modify(GayPauseLayer, PauseLayer) {
-    void onGaySettings(CCObject*) { // NOLINT(*-convert-member-functions-to-static)
-        geode::openSettingsPopup(Mod::get(), false);
+  void onGaySettings(CCObject *) {
+    geode::openSettingsPopup(Mod::get(), false);
+  }
+
+  void customSetup() override {
+    PauseLayer::customSetup();
+
+    if (gay::settings::get<bool>("enable-pause-btn")) {
+      auto settingsButtonSprite = CCSprite::create("button.png"_spr);
+      settingsButtonSprite->setScale(.25f);
+      if (auto betterPause = Loader::get()->getLoadedMod("tpdea.betterpause-Better")) {
+        if (betterPause->getSettingValue<int64_t>("type-pause") == 1)
+          settingsButtonSprite->setScale(.95f);
+      }
+
+      auto settingsBtn = CCMenuItemSpriteExtra::create(settingsButtonSprite, this, menu_selector(GayPauseLayer::onGaySettings));
+
+      settingsBtn->setID("gay-settings-button"_spr);
+      if (auto leftMenu = getChildByIDRecursive("left-button-menu")) {
+        leftMenu->addChild(settingsBtn);
+        leftMenu->updateLayout();
+      }
     }
-
-    void customSetup() override {
-        PauseLayer::customSetup();
-
-        auto settingsButtonSprite = CCSprite::create("button.png"_spr);
-        settingsButtonSprite->setScale(.25f);
-        if (auto betterPause = Loader::get()->getLoadedMod("tpdea.betterpause-Better")) {
-            if (betterPause->getSettingValue<int64_t>("type-pause") == 1) settingsButtonSprite->setScale(.95f);
-        }
-
-        auto settingsBtn = CCMenuItemSpriteExtra::create(
-                settingsButtonSprite,
-                this,
-                menu_selector(GayPauseLayer::onGaySettings)
-        );
-
-        settingsBtn->setID("gay-settings-button"_spr);
-        if (auto leftMenu = getChildByIDRecursive("left-button-menu")) {
-            leftMenu->addChild(settingsBtn);
-            leftMenu->updateLayout();
-        }
-    }
+  }
 };
