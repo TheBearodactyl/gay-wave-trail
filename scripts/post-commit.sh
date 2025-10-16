44a716d70b4393004c12103221d7664263e54570
The Bearodactyl <dabewodactowluwu@gmail.com>
2025-10-16 17:29:26 -0500

    update changelog script and changelog

--- scripts/post-commit.sh
+++ scripts/post-commit.sh
@@ -1,16 +1,33 @@
 #!/bin/sh
 # Copyright (c) 2013 Martin Seeler
 # SPDX-License-Identifier: MIT
-# taken from https://github.com/MartinSeeler/auto-changelog-hook/blob/master/post-commit
+# edited version of https://github.com/MartinSeeler/auto-changelog-hook/blob/master/post-commit
 
 OUTPUT_FILE=CHANGELOG.md
 
-git --no-pager log --no-merges --format="### %s%d%n>%aD%n%n>Author: %aN (%aE)%n%n>Commiter: %cN (%cE)%n%n%b%n%N%n" > $OUTPUT_FILE
+TEMP_FILE=$(mktemp)
 
-res=$(git status --porcelain | grep -c ".\$OUTPUT_FILE$")
+cat > $TEMP_FILE << 'EOF'
+# Changelog
+
+## [Unreleased]
+
+EOF
+
+git --no-pager log --no-merges --reverse --format="### %s%n%n**Date:** %aD  \n**Author:** %aN (%aE)  \n**Commit Hash:** %H  \n%n%b%n%n---%n" >> $TEMP_FILE
+
+if [ -f "$OUTPUT_FILE" ]; then
+	tail -n +2 "$OUTPUT_FILE" >> $TEMP_FILE 2>/dev/null || true
+fi
+
+mv $TEMP_FILE $OUTPUT_FILE
+
+res=$(git status --porcelain | grep -c "$OUTPUT_FILE")
 if [ "$res" -gt 0 ]; then
 	git add $OUTPUT_FILE
-	git commit --amend
+	git commit --amend --no-edit
 
 	echo "Populated Changelog in $OUTPUT_FILE"
+else
+	echo "No changes to $OUTPUT_FILE"
 fi
